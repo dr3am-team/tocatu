fe477229b58344f3c4258d9b9619c9a6b60a8b58
Pedro Kupersmit <pkupersmit@gmail.com>
2022-10-31 22:36:49 -0300

    agregado footer, navbar y modelo de tarjeta

--- tocatu/Migrations/20221101013107_Tocatu.Context.TocatuDBContext.Designer.cs
+++ tocatu/Migrations/20221101013107_Tocatu.Context.TocatuDBContext.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using tocatu.Context;
 
 namespace tocatu.Migrations
 {
     [DbContext(typeof(TocatuContext))]
-    partial class TocatuContextModelSnapshot : ModelSnapshot
+    [Migration("20221101013107_Tocatu.Context.TocatuDBContext")]
+    partial class TocatuContextTocatuDBContext
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
