1c1d58e54c964f5e72de98ac4c51ad29399de88e
Pedro <pkupersmit@gmail.com>
2022-11-18 19:30:38 -0300

    Agregado List() e Index()

--- tocatu/Controllers/EventoController.cs
+++ tocatu/Controllers/EventoController.cs
@@ -19,16 +19,16 @@ namespace tocatu.Controllers
       _context = context;
     }
 
-    // GET: Evento
     public async Task<IActionResult> Index()
     {
-      return View(await _context.Eventos.ToListAsync());
+      return View();
     }
-
+    // GET: Evento
     public async Task<IActionResult> List()
     {
       return View(await _context.Eventos.ToListAsync());
     }
+
 
 
     // GET: Evento/Details/5
@@ -67,7 +67,7 @@ namespace tocatu.Controllers
       var fecha = evento.Dia.Split("-");
       DateTime _fecha = new DateTime(int.Parse(fecha[0]), int.Parse(fecha[1]), int.Parse(fecha[2]));
       evento.Fecha = _fecha;
-      
+
 
 
 
@@ -112,7 +112,7 @@ namespace tocatu.Controllers
       {
         return NotFound();
       }
-            ObtenerListaDeBandas();
+      ObtenerListaDeBandas();
       var evento = await _context.Eventos.FindAsync(id);
       if (evento == null)
       {
@@ -121,91 +121,91 @@ namespace tocatu.Controllers
       return View(evento);
     }
 
-        // POST: Evento/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(int id, [Bind("EventId,Nombre,Descripcion,PrecioEntrada,Dia,Hora,Capacidad,Direccion,BarId")] Evento evento)
-        //{
-        //  if (id != evento.EventId)
-        //  {
-        //    return NotFound();
-        //  }
+    // POST: Evento/Edit/5
+    // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+    // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    //[HttpPost]
+    //[ValidateAntiForgeryToken]
+    //public async Task<IActionResult> Edit(int id, [Bind("EventId,Nombre,Descripcion,PrecioEntrada,Dia,Hora,Capacidad,Direccion,BarId")] Evento evento)
+    //{
+    //  if (id != evento.EventId)
+    //  {
+    //    return NotFound();
+    //  }
 
-        //  if (ModelState.IsValid)
-        //  {
-        //    try
-        //    {
-        //      _context.Update(evento);
-        //      await _context.SaveChangesAsync();
-        //    }
-        //    catch (DbUpdateConcurrencyException)
-        //    {
-        //      if (!EventoExists(evento.EventId))
-        //      {
-        //        return NotFound();
-        //      }
-        //      else
-        //      {
-        //        throw;
-        //      }
-        //    }
-        //    return RedirectToAction(nameof(Index));
-        //  }
-        //  return View(evento);
-        //}
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("EventId,Nombre,Descripcion,PrecioEntrada,Dia,Hora, BarId, BandaId")] Evento evento)
+    //  if (ModelState.IsValid)
+    //  {
+    //    try
+    //    {
+    //      _context.Update(evento);
+    //      await _context.SaveChangesAsync();
+    //    }
+    //    catch (DbUpdateConcurrencyException)
+    //    {
+    //      if (!EventoExists(evento.EventId))
+    //      {
+    //        return NotFound();
+    //      }
+    //      else
+    //      {
+    //        throw;
+    //      }
+    //    }
+    //    return RedirectToAction(nameof(Index));
+    //  }
+    //  return View(evento);
+    //}
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("EventId,Nombre,Descripcion,PrecioEntrada,Dia,Hora, BarId, BandaId")] Evento evento)
+    {
+      if (id != evento.EventId)
+      {
+        return NotFound();
+      }
+      //establezco los datos de la banda en el evento
+      var Banda = from banda in _context.Usuarios
+                  where (banda.UserId == evento.BandaId)
+                  select banda;
+      var banda1 = (Banda)Banda.FirstOrDefault();
+      evento.NombreBanda = banda1.Nombre;
+      evento.EstiloBanda = banda1.Estilo;
+      //establezco los datos del bar en el evento para que no se pierdan al editar
+      var Bar = from bar in _context.Usuarios
+                where (bar.UserId == evento.BarId)
+                select bar;
+
+      var Bar1 = (Bar)Bar.FirstOrDefault();
+      evento.Capacidad = Bar1.Capacidad;
+      evento.Direccion = Bar1.Direccion;
+
+
+
+      if (ModelState.IsValid)
+      {
+        try
         {
-            if (id != evento.EventId)
-            {
-                return NotFound();
-            }
-            //establezco los datos de la banda en el evento
-            var Banda = from banda in _context.Usuarios
-                        where (banda.UserId == evento.BandaId)
-                        select banda;
-            var banda1 = (Banda)Banda.FirstOrDefault();
-            evento.NombreBanda = banda1.Nombre;
-            evento.EstiloBanda = banda1.Estilo;
-            //establezco los datos del bar en el evento para que no se pierdan al editar
-            var Bar = from bar in _context.Usuarios
-                      where (bar.UserId == evento.BarId)
-                      select bar;
-            
-            var Bar1 = (Bar)Bar.FirstOrDefault();
-            evento.Capacidad = Bar1.Capacidad;
-            evento.Direccion= Bar1.Direccion;
-
-
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(evento);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!EventoExists(evento.EventId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(evento);
+          _context.Update(evento);
+          await _context.SaveChangesAsync();
         }
+        catch (DbUpdateConcurrencyException)
+        {
+          if (!EventoExists(evento.EventId))
+          {
+            return NotFound();
+          }
+          else
+          {
+            throw;
+          }
+        }
+        return RedirectToAction(nameof(Index));
+      }
+      return View(evento);
+    }
 
-        // GET: Evento/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+    // GET: Evento/Delete/5
+    public async Task<IActionResult> Delete(int? id)
     {
       if (id == null)
       {
@@ -238,113 +238,111 @@ namespace tocatu.Controllers
       return _context.Eventos.Any(e => e.EventId == id);
     }
 
-        /* private void PopulateBaresDropDownList(int? selectedBar = null)
-         {
-             var bares = _context.PopulateBaresDropDownList();
-             ViewBag.UserId = new SelectList(bares.AsNoTracking(), "UserId", "UserNombre", selectedBar);
-         }*/
-        public void ObtenerListaDeBares()
-        {
-            //establecemos los bares en el viewbag para que aparezcan en el desplegable
-            var Usuarios = _context.Usuarios.ToList();
+    /* private void PopulateBaresDropDownList(int? selectedBar = null)
+     {
+         var bares = _context.PopulateBaresDropDownList();
+         ViewBag.UserId = new SelectList(bares.AsNoTracking(), "UserId", "UserNombre", selectedBar);
+     }*/
+    public void ObtenerListaDeBares()
+    {
+      //establecemos los bares en el viewbag para que aparezcan en el desplegable
+      var Usuarios = _context.Usuarios.ToList();
 
-            var Bares = from usuario in Usuarios
-                        where usuario is Bar
-                        select usuario;
-
-
-            ViewBag.Bar = new SelectList(Bares, "UserId", "Nombre");
-
-        }
-        public void ObtenerListaDeBandas()
-        {
-            //establecemos los bandas en el viewbag para que aparezcan en el desplegable
-            var Usuarios = _context.Usuarios.ToList();
-
-            var Bandas = from usuario in Usuarios
-                         where usuario is Banda
-                         select usuario;
+      var Bares = from usuario in Usuarios
+                  where usuario is Bar
+                  select usuario;
 
 
-            ViewBag.Banda = new SelectList(Bandas, "UserId", "Nombre");
+      ViewBag.Bar = new SelectList(Bares, "UserId", "Nombre");
 
-        }
-        public void ActualizarDatos(int capacidad, string direccion, int id)
-        {
-            //buscar al evento del id
-            var Evento = from evento in _context.Eventos
-                        where (evento.BarId == id)
-                        select evento;
-            foreach (Evento e in Evento)
-            {
-                e.Capacidad = capacidad;
-                e.Direccion = direccion;
+    }
+    public void ObtenerListaDeBandas()
+    {
+      //establecemos los bandas en el viewbag para que aparezcan en el desplegable
+      var Usuarios = _context.Usuarios.ToList();
 
-            }
+      var Bandas = from usuario in Usuarios
+                   where usuario is Banda
+                   select usuario;
 
 
+      ViewBag.Banda = new SelectList(Bandas, "UserId", "Nombre");
 
-            var Evento1 = Evento.FirstOrDefault();
-            Evento1.Capacidad= capacidad;
-            Evento1.Direccion= direccion;
-        }
-        public void ActualizarDatos(string estilo, string nombre, int id)
-        {
-            //buscar al evento del id
-            var Evento = from evento in _context.Eventos
-                         where (evento.BandaId == id)
-                         select evento;
+    }
+    public void ActualizarDatos(int capacidad, string direccion, int id)
+    {
+      //buscar al evento del id
+      var Evento = from evento in _context.Eventos
+                   where (evento.BarId == id)
+                   select evento;
+      foreach (Evento e in Evento)
+      {
+        e.Capacidad = capacidad;
+        e.Direccion = direccion;
 
-            foreach(Evento e in Evento)
-            {
-                e.EstiloBanda = estilo;
-                e.NombreBanda = nombre;
+      }
 
-            }
-            //var Evento1 = Evento.FirstOrDefault();
-            //Evento1.EstiloBanda = estilo;
-            //Evento1.NombreBanda = nombre;
-        }
-        public void BorrarEventosAsociados(int id)
-        {
-            var Evento = from evento in _context.Eventos
-                         where (evento.BarId == id)
-                         select evento;
 
-            foreach (Evento e in Evento)
-            {
-                DeleteConfirmed(e.EventId);
-               // e.BarId = null;
 
-            }
-        }
-        public void BorrarIdBanda(int id)
-        {
-            var Evento = from evento in _context.Eventos
-                         where (evento.BandaId == id)
-                         select evento;
+      var Evento1 = Evento.FirstOrDefault();
+      Evento1.Capacidad = capacidad;
+      Evento1.Direccion = direccion;
+    }
+    public void ActualizarDatos(string estilo, string nombre, int id)
+    {
+      //buscar al evento del id
+      var Evento = from evento in _context.Eventos
+                   where (evento.BandaId == id)
+                   select evento;
 
-            foreach (Evento e in Evento)
-            {
-                e.BandaId = null;
+      foreach (Evento e in Evento)
+      {
+        e.EstiloBanda = estilo;
+        e.NombreBanda = nombre;
 
-            }
-        }
+      }
+      //var Evento1 = Evento.FirstOrDefault();
+      //Evento1.EstiloBanda = estilo;
+      //Evento1.NombreBanda = nombre;
+    }
+    public void BorrarEventosAsociados(int id)
+    {
+      var Evento = from evento in _context.Eventos
+                   where (evento.BarId == id)
+                   select evento;
 
-        public void EstablecerDatosDeBandaVacios(int id)
-        {
-            var Evento = from evento in _context.Eventos
-                         where (evento.BandaId == id)
-                         select evento;
+      foreach (Evento e in Evento)
+      {
+        DeleteConfirmed(e.EventId);
+        // e.BarId = null;
 
-            foreach (Evento e in Evento)
-            {
-                e.NombreBanda = "";
-                e.EstiloBanda = "";
+      }
+    }
+    public void BorrarIdBanda(int id)
+    {
+      var Evento = from evento in _context.Eventos
+                   where (evento.BandaId == id)
+                   select evento;
 
-            }
-        }
+      foreach (Evento e in Evento)
+      {
+        e.BandaId = null;
+
+      }
     }
 
+    public void EstablecerDatosDeBandaVacios(int id)
+    {
+      var Evento = from evento in _context.Eventos
+                   where (evento.BandaId == id)
+                   select evento;
+
+      foreach (Evento e in Evento)
+      {
+        e.NombreBanda = "";
+        e.EstiloBanda = "";
+
+      }
+    }
   }
 }
