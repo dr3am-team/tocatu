e1cc46e0f960a0a992dd7b7ab324b27909c2edf6
felor <felodrobles@gmail.com>
2022-11-27 22:09:19 -0300

    capacidades --

--- tocatu/Controllers/HomeController.cs
+++ tocatu/Controllers/HomeController.cs
@@ -11,36 +11,38 @@ using Microsoft.EntityFrameworkCore;
 
 namespace tocatu.Controllers
 {
-  public class HomeController : Controller
-  {
-    private readonly ILogger<HomeController> _logger;
-    private readonly TocatuContext _context;
-
-    public HomeController(ILogger<HomeController> logger, TocatuContext context)
+    public class HomeController : Controller
     {
-      _logger = logger;
-      _context = context;
-    }
+        private readonly ILogger<HomeController> _logger;
+        private readonly TocatuContext _context;
 
-    //public IActionResult Index()
-    //{
-    //  return View();
-    //}
+        public HomeController(ILogger<HomeController> logger, TocatuContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
 
-    public async Task<IActionResult> Index()
-    {
-      return View(await _context.Eventos.ToListAsync());
-    }
+        //public IActionResult Index()
+        //{
+        //  return View();
+        //}
 
-    public IActionResult Privacy()
-    {
-      return View();
-    }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Eventos.ToListAsync());
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
     }
-  }
 }
